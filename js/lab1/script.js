3762f26f122cd169b54c72041fe51441f2d539c9
BeliyDot <artemonchannel2005@gmail.com>
2025-03-07 12:19:10 +0200

    Change onclick attribute to addEventListener in script.js

--- js/lab1/script.js
+++ js/lab1/script.js
@@ -1,7 +1,10 @@
 const header = document.getElementById('header');
+const button = document.getElementById('btn');
 
 header.innerText = "Hello World!";
 
 function clickButton() {
     console.log("Artem");
 }
+
+button.addEventListener("click", clickButton);
