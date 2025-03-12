eeb88e39f1839113132de20486a5754a88d8ac40
BeliyDot <artemonchannel2005@gmail.com>
2025-03-12 11:55:47 +0200

    Change task1.js to remove unnecessary code

--- js/lab2/task1.js
+++ js/lab2/task1.js
@@ -1,5 +1,5 @@
 function returnMaxAndMin(arr) {
-    let max = undefined, min = undefined;
+    let max, min;
     for(let i = 0; i < arr.length; i++) {
         if(!max || arr[i] > max) max = arr[i];
         if(!min || arr[i] < min) min = arr[i];
