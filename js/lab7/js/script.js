b69b7f9b173c93fd75df799896b3c4e9cc8332f7
BeliyDot <artemonchannel2005@gmail.com>
2025-04-18 11:21:27 +0300

    Add live system, display reward and add option to 'miss the shot'

--- js/lab7/js/script.js
+++ js/lab7/js/script.js
@@ -6,6 +6,7 @@ let score = 0,
     reward = 3000,
     bonus = 0,
     level = 1,
+    lives = 3,
     gunmanTime = 150;
 
 const buttonStart = document.querySelector('.button-start-game'),
@@ -18,6 +19,8 @@ const buttonStart = document.querySelector('.button-start-game'),
     timePanelGunman = document.querySelector('.time-panel__gunman'),
     timePanelPlayer = document.querySelector('.time-panel__you'),
     scorePanelNumber = document.querySelector('.score-panel__score_num'),
+    rewardPanelNumber = document.querySelector('.score-panel__reward_num'),
+    livesPanelNumber = document.querySelector('.score-panel__lives_num'),
     levelPanelNumber = document.querySelector('.score-panel__level_num'),
     gunman = document.querySelector('.gunman'),
     hat = document.querySelector('.hat'),
@@ -32,13 +35,20 @@ const buttonStart = document.querySelector('.button-start-game'),
     waitSound = new Audio('sfx/wait.m4a');
 
 
+function updateStats() {
+    scorePanelNumber.textContent = score;
+    rewardPanelNumber.textContent = reward;
+    levelPanelNumber.textContent = level;
+    livesPanelNumber.textContent = lives;
+    timePanelGunman.textContent = (gunmanTime / 100).toFixed(2);
+    timePanelPlayer.textContent = '0.00';
+}
+
 function startGame() {
     level = 1;
     score = 0;
-    scorePanelNumber.textContent = score;
-    levelPanelNumber.textContent = level;
     gameMenu.style.display = 'none';
-    timePanelGunman.textContent = (gunmanTime / 100).toFixed(2);
+    updateStats();
     gunman.classList.add(`gunman-level-${level}`);
     setTimeout(() => {
         gameWrapper.style.display = 'block';
@@ -67,10 +77,17 @@ function restartGame() {
 
 buttonRestart.onclick = restartGame;
 
+function gameOver() {
+    message.style.display = 'none';
+    gameWrapper.style.display = 'none';
+    setTimeout(() => {
+        gameMenu.style.display = 'block';
+    }) 
+}
+
 function nextLevel() {
     winSound.pause();
     gameWrapper.style.display = 'none';
-    timePanelPlayer.textContent = '0.00';
     buttonNext.style.display = 'none';
     gunman.classList.remove(`gunman-level-${level}__death`);
     gameScreen.classList.remove('game-screen--win');
@@ -85,10 +102,9 @@ function nextLevel() {
         return;
     }
     gunman.classList.add(`gunman-level-${level}`);
-    levelPanelNumber.textContent = level;
     gunmanTime -= 30;
-    timePanelGunman.textContent = (gunmanTime / 100).toFixed(2);
     reward += 500;
+    updateStats();
     setTimeout(() => {
         gameWrapper.style.display = 'block';
         moveGunman();
@@ -113,8 +129,8 @@ function prepareForDuel() {
     waitSound.loop = true;
     waitSound.currentTime = 0;
     waitSound.play();
-    gunman.classList.toggle(`gunman-level-${level}__standing`)
-    gunman.classList.toggle('shootable');
+    gunman.classList.toggle(`gunman-level-${level}__standing`);
+    gameScreen.classList.toggle('shootable');
 
     const timeoutId = setTimeout(() => {
         gunman.classList.toggle(`gunman-level-${level}__standing`)
@@ -122,12 +138,22 @@ function prepareForDuel() {
         timeCounter();
     }, timeToWait);
     
-    gunman.onclick = () => {
-        gunman.onclick = null;
-        gunman.classList.toggle('shootable');
+    gameScreen.onclick = () => {
+        gameScreen.onclick = null;
+        gameScreen.classList.toggle('shootable');
         clearTimeout(timeoutId);
         playerShootsEarly();
     };
+}
+
+function screenShot() {
+    shotPlayerSound.currentTime = 0;
+    shotPlayerSound.play();
+    gameScreen.classList.toggle('game-screen--shot');
+    setTimeout(() => {
+        gameScreen.classList.toggle('game-screen--shot');
+        gameScreen.onclick = screenShot;
+    }, 500);
 }
 
 function timeCounter() {
@@ -143,24 +169,33 @@ function timeCounter() {
         if(playerTime > gunmanTime) {
             message.classList.toggle('message--fire');
             gunman.onclick = null;
+            gameScreen.onclick = null;
             clearInterval(intervalId);
             gunman.classList.toggle(`gunman-level-${level}__ready`)
-            gunman.classList.toggle('shootable');
             gunmanShootsPlayer();
         }
     }, 10);
+    gameScreen.onclick = screenShot;
     gunman.onclick = () => {
         gunman.onclick = null;
         message.classList.toggle('message--fire');
         clearInterval(intervalId);
         bonus = ((gunmanTime / 10) - Math.floor(playerTime / 10)) * 1000;
         gunman.classList.toggle(`gunman-level-${level}__ready`)
-        gunman.classList.toggle('shootable');
+        setTimeout(() => {
+            gameScreen.onclick = null;
+        }, 501);
         playerShootsGunman();
     }
 }
 
 function gunmanShootsPlayer() {
+    gunman.classList.remove('shootable');
+    gameScreen.classList.remove('shootable');
+    lives--;
+    score -= reward;
+    livesPanelNumber.textContent = lives;
+    scorePanelNumber.textContent = score;
     shotPlayerSound.currentTime = 0;
     shotPlayerSound.play();
     gunman.classList.toggle(`gunman-level-${level}__shooting`);
@@ -168,16 +203,30 @@ function gunmanShootsPlayer() {
     deathSound.currentTime = 0;
     deathSound.play();
     gameScreen.classList.toggle('game-screen--death');
-    setTimeout(() => {
-        message.classList.toggle('message--dead');
-        message.textContent = 'You are ded.';
-    }, 3000);
-    setTimeout(() => {
-        buttonRestart.style.display = 'block';
-    }, 4000);
+    if(lives > 0) {
+        setTimeout(() => {
+            message.classList.toggle('message--dead');
+            message.textContent = 'You lost.';
+        }, 3000);
+        setTimeout(() => {
+            buttonRestart.style.display = 'block';
+        }, 4000);
+    }
+    else {
+        setTimeout(() => {
+            message.classList.toggle('message--dead');
+            message.textContent = 'You are ded.';
+        }, 3000);
+        setTimeout(() => {
+            message.textContent += '\nGame over.'
+        }, 7000);
+        setTimeout(gameOver, 9000);
+    }
 }
 
 function playerShootsGunman() {
+    gunman.classList.remove('shootable');
+    gameScreen.classList.remove('shootable');
     if(level == 1 || level >= 4) {
         shotGunmanSound.currentTime = 0;
         shotGunmanSound.play();
@@ -207,6 +256,8 @@ function playerShootsGunman() {
 }
 
 function playerShootsEarly() {
+    lives -= 1;
+    livesPanelNumber.textContent = lives;
     waitSound.pause();
     foulSound.currentTime = 0;
     foulSound.play();
@@ -216,11 +267,21 @@ function playerShootsEarly() {
     const intervalId = setInterval(() => {
         message.style.visibility = message.style.visibility == 'visible' ? 'hidden' : 'visible';
     }, 100);
-    setTimeout(() => {
-        clearInterval(intervalId);
-        message.style.visibility = 'visible';
-        buttonRestart.style.display = 'block';
-    }, 1500);
+    if(lives > 0) {
+        setTimeout(() => {
+            clearInterval(intervalId);
+            message.style.visibility = 'visible';
+            buttonRestart.style.display = 'block';
+        }, 1500);
+    }
+    else {
+        setTimeout(() => {
+            clearInterval(intervalId);
+            message.style.visibility = 'visible';
+            message.textContent += '\nGame over.'
+        }, 1500);
+        setTimeout(gameOver, 2500);
+    }
 }
 
 function scoreCount() {
