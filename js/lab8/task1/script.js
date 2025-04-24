df8ee58ac73bc91fa7ca023f93296883ff0c8539
BeliyDot <artemonchannel2005@gmail.com>
2025-04-25 02:18:24 +0300

    Update image URLs

--- js/lab8/task1/script.js
+++ js/lab8/task1/script.js
@@ -28,7 +28,7 @@ const images = [
     { name: "evw", url: "https://media1.tenor.com/m/morPeOgGlkkAAAAd/erivvanwilderman-geometry-dash.gif" },
     { name: "normal", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgUbk38tKTnRvyJdoId2c3UqTxrUcbUzPoKQ&s" },
     { name: "colon", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiSaR3LSzCYXw5ZlBdQ9Tih_a9FB917503ig&s" },
-    { name: "michigun", url: "https://static.wikia.nocookie.net/geometry-dash/images/f/f8/Michigun.jpg/revision/latest?cb=20161227142259&path-prefix=ru" },
+    { name: "michigun", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTJXzfCCqVe452VmMY2ii23piKq7pCViXWFA&s" },
     { name: "robtop", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWi8B8p0ccTrbLuNT5STlbvaoDKJ_kfn4yhg&s" },
     { name: "boomkitty", url: "https://i.scdn.co/image/ab676161000051740537e4e52505684d70275b45" },
     { name: "waterflame", url: "https://yt3.googleusercontent.com/vYbhw3MueeU3uaf-gIgx1ybgrJs_lqxrZgiUMRyphhstfBV_wReu4drozput5YcA7y2WL4g3Yw=s900-c-k-c0x00ffffff-no-rj" },
@@ -36,11 +36,11 @@ const images = [
     { name: "decody", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJXcgCP_lCfczzruOcxs_XS82bOz18CQ1M6Q&s" },
     { name: "vortrox", url: "https://i.ytimg.com/vi/W4S9d3xCGMI/sddefault.jpg" },
     { name: "bloodbath", url: "https://i.ytimg.com/vi/LjlkMdZ9eJ0/maxresdefault.jpg" },
-    { name: "sammelot", url: "https://static.wikia.nocookie.net/youtube/images/b/b2/KingSammelotClips.jpg/revision/latest/thumbnail/width/360/height/360?cb=20240914055936" },
+    { name: "sammelot", url: "https://static.wikia.nocookie.net/youtube/images/b/b2/KingSammelotClips.jpg" },
     { name: "dash", url: "https://i.redd.it/hjcrljvkpgec1.jpeg" },
-    { name: "congregation", url: "https://static.wikia.nocookie.net/geometry-dash-fan/images/7/7e/Congregation.jpg/revision/latest?cb=20250119211837" },
+    { name: "congregation", url: "https://static.wikia.nocookie.net/geometry-dash-fan/images/7/7e/Congregation.jpg" },
     { name: "vortex", url: "https://preview.redd.it/pc1c9a2598t91.png?width=640&crop=smart&auto=webp&s=7a2d813f1d769dc1db723a30dee9f6011013fd1b" },
-    { name: "maffaka", url: "https://static.wikia.nocookie.net/geometry-dash/images/2/2b/MoFFoKo.jpg/revision/latest?cb=20210717173100&path-prefix=ru" },
+    { name: "maffaka", url: "https://pbs.twimg.com/profile_images/1333109176785129472/ChjBA7UG_400x400.jpg" },
     { name: "vernam", url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFbwrXagI8pyv2u8Ymh4GldwhqsugCTeulxQ&s" },
     { name: "limbo", url: "https://external-preview.redd.it/i-made-the-limbo-keys-in-3d-guess-which-one-is-the-correct-v0-b3FvbHcxaW53cXhjMUL-bknpD9KqUlrx0iBtsTuauBRzIUrYAJ0fUNC7tP5Z.png?format=pjpg&auto=webp&s=635941593e86a2011045d81a2a90f7d1548992df" }
 ];
